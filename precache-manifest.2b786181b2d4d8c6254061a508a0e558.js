20934cda77235e46feb3ae8148202b1342052804
José Ángel Bermúdez Puello <jbermudezp>
2019-12-01 00:26:09 -0500

    Updates

---
+++ precache-manifest.2b786181b2d4d8c6254061a508a0e558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0ec3756118e745c0a5358071afb2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0df9bc287d1a18ece46",
+    "url": "/static/css/main.bcaa4045.chunk.css"
+  },
+  {
+    "revision": "4ca9c0d1dc497fc52c2c",
+    "url": "/static/js/2.e24468eb.chunk.js"
+  },
+  {
+    "revision": "d0df9bc287d1a18ece46",
+    "url": "/static/js/main.c3d798c4.chunk.js"
+  },
+  {
+    "revision": "d67270fb8c2f69c8cf72",
+    "url": "/static/js/runtime-main.39e34ecb.js"
+  }
+]);
